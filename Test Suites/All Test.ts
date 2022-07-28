aa788ffadd9269677a2b0bf8fd56ebe174f954eb
CitraCamila <citracamila@gmail.com>
2022-07-28 19:02:43 +0700

    commit1

---
+++ Test Suites/All Test.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>All Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>af20d4c3-48a1-44e4-9a78-549424cd45da</testSuiteGuid>
+   <testCaseLink>
+      <guid>e82d9e18-9f91-4845-b643-56d83167b50d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Get</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>19d08c98-b7c6-4e15-8c99-1ee5f2e52b14</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Post</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>71c869af-b39c-4c0f-818c-325df604af63</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Update</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a58bdcd-d706-4507-9a11-83cec06ed65d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Delete</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
